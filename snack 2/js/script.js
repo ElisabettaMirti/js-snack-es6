c6855cb0f42de9ce193a7f08739cb89e0f4f474a
ElisabettaMirti <belmirti@gmail.com>
2024-05-23 17:47:17 +0200

    inserita lista dei nomi in maiuscolo

--- snack 2/js/script.js
+++ snack 2/js/script.js
@@ -41,6 +41,10 @@ const listaStudenti = [
 
 //creo la prima lista: creao una variabile per un array che contenga tutti i nomi e li stampi in console in maiuscolo
 
+const nomiTarghe = listaStudenti.map((studente) => studente.Name.toUpperCase());
+console.log(nomiTarghe);
+
+
 //creo un altro array per filtrare tutti gli studenti che hanno un totale di voti superiore a 70
 
 // creo un altro array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
