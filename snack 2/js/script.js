9ecfc6ac5a8cfae07fd4450beb38f016a55a8545
ElisabettaMirti <belmirti@gmail.com>
2024-05-23 17:32:09 +0200

    inserito pseudocodice di base

---
+++ snack 2/js/script.js
@@ -0,0 +1,8 @@
+//creo un array di oggetti con tutta la lista studenti, il loro id e la loro media
+
+//creo la prima lista: creao una variabile per un array che contenga tutti i nomi e li stampi in console in maiuscolo
+
+//creo un altro array per filtrare tutti gli studenti che hanno un totale di voti superiore a 70
+
+// creo un altro array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+
