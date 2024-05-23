dc3ecee56d65db1bcf0072e5f595fed6d582679e
ElisabettaMirti <belmirti@gmail.com>
2024-05-23 17:26:35 +0200

    finito snack 1

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -3,6 +3,16 @@
 const listaVip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
 
 
-//uso un ciclo (vediamo quale) per creare un array di oggetti con i nomi, l'ordine giusto, e il loro tavolo
+//uso un map per creare un array di oggetti con i nomi, l'ordine giusto, e il loro tavolo
+
+const listaInvitati = listaVip.map((guest, indice) => ({
+    nome: guest,
+    posto: indice + 1,
+    tavolo: "Tavolo vip"
+}))
+
+
 
 //stampo in console
+console.log(listaInvitati);
+
