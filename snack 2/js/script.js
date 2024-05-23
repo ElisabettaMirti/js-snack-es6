be9aa15bcc03a7d446e2b83874e5ed9bfa51e692
ElisabettaMirti <belmirti@gmail.com>
2024-05-23 17:37:10 +0200

    inserita lista di oggetti per i dati degli studenti

--- snack 2/js/script.js
+++ snack 2/js/script.js
@@ -1,8 +1,46 @@
 //creo un array di oggetti con tutta la lista studenti, il loro id e la loro media
 
+const listaStudenti = [
+    {
+        Id: 213,
+        Name: "Giuseppina della Rovere", 
+        Grades: 78,
+    },
+    {
+        Id: 110,
+        Name: "Paola Cortellessa",
+        Grades: 96
+    },
+    {
+        Id: 250,
+        Name: "Andrea Mantegna",
+        Grades: 48
+    },
+    {
+        Id: 145,
+        Name: "Gaia Borromini",
+        Grades: 74
+    },
+    {
+        Id: 196, 
+        Name: "Luigi Grimaldello",
+        Grades: 68
+    },
+    {
+        Id: 102,
+        Name: "Piero della Francesca",
+        Grades: 50
+    },
+    {
+        Id: 120,
+        Name: "Francesca da Polenta",
+        Grades: 84
+    }
+]
+
+
 //creo la prima lista: creao una variabile per un array che contenga tutti i nomi e li stampi in console in maiuscolo
 
 //creo un altro array per filtrare tutti gli studenti che hanno un totale di voti superiore a 70
 
 // creo un altro array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
-
