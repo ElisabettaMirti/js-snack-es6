9c16dd26f77c83ddcbe6456ae8ef471808f3d8be
ElisabettaMirti <belmirti@gmail.com>
2024-05-23 16:49:11 +0200

    inserito pseudocodice di base

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// ceo l'array con i nomi degli ospiti
+
+const listaVip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
+
+
+//uso un ciclo (vediamo quale) per creare un array di oggetti con i nomi, l'ordine giusto, e il loro tavolo
+
+//stampo in console
