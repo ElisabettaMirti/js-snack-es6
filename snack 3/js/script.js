24712bb996325c181d2a0adb416c01c4deb750b4
ElisabettaMirti <belmirti@gmail.com>
2024-05-26 10:03:52 +0200

    inserito pseudocodice di base e inserito array di oggetti bici

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -1,3 +1,62 @@
 // > Creare un array di oggetti:
 // > Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // > Stampare in console la bici con peso minore utilizzando destructuring e template literal
+
+
+//creo l'array di oggetti per le bici 
+const biciclette = [
+    { 
+        nome: "Wilier Triestina Zero SLR", 
+        peso: 6.6 
+    },
+
+    { 
+        nome: "BMC Teammachine SLR01", 
+        peso: 6.9 
+    },
+
+    { 
+        nome: "De Rosa SK Pininfarina", 
+        peso: 6.8 
+    },
+
+    { 
+        nome: "Factor O2 VAM", 
+        peso: 6.7 
+    },
+
+    { 
+        nome: "Orbea Orca OMX", 
+        peso: 7.0 
+    },
+
+    { 
+        nome: "Canyon Ultimate CF SLX", 
+        peso: 6.8 
+    },
+
+    { 
+        nome: "Lapierre Aircode DRS", 
+        peso: 7.1 
+    },
+
+    { 
+        nome: "Ridley Noah Fast", 
+        peso: 7.0 
+    },
+
+    { 
+        nome: "Argon 18 Gallium Pro", 
+        peso: 6.9 
+    },
+
+    { 
+        nome: "Look 785 Huez RS", 
+        peso: 6.7 
+    }
+];
+
+//creo un ciclo forEach per verificare il peso di ogni oggetto-bicicletta
+
+//stampo in console il risultato con il template literal
+
